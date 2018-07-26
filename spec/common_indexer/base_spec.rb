5001b626793b6f08f8d03751d026e3a70d0e3e51
Michael B. Klein <mbklein@gmail.com>
2018-07-26 14:52:36 -0500

    Normalize base specs
    Co-authored-by: Brendan Quinn <brendan-quinn@northwestern.edu>

--- spec/common_indexer/base_spec.rb
+++ spec/common_indexer/base_spec.rb
@@ -3,54 +3,49 @@
 require 'spec_helper'
 
 RSpec.describe CommonIndexer::Base do
-  let(:resource) { TestModel.new }
+  let(:resource) { TestModel.new(title: ['Main Title'], description: ['Short Description']) }
+  let(:index)    { resource.to_common_index }
+
+  before do
+    class TestModel < ActiveFedora::Base
+      include CommonIndexer::Base
+
+      property :title, predicate: ::RDF::URI('http://example.org/ns#title')
+      property :description, predicate: ::RDF::URI('http://example.org/ns#description')
+      property :collection, predicate: ::RDF::URI('http://example.org/ns#collection')
+      property :thumbnail, predicate: ::RDF::URI('http://example.org/ns#thumbnail')
+      property :poster, predicate: ::RDF::URI('http://example.org/ns#poster')
+      property :resource_type, predicate: ::RDF::URI('http://example.org/ns#resource_type')
+    end
+  end
+
+  after do
+    Object.send(:remove_const, :TestModel)
+  end
+
+  it 'includes CommonIndexer::Base' do
+    expect(TestModel.ancestors).to include(described_class)
+  end
 
   context 'not implemented' do
-    before do
-      class TestModel < ActiveFedora::Base
-        include CommonIndexer::Base
-      end
-    end
-
-    after do
-      Object.send(:remove_const, :TestModel)
-    end
-
     it '#to_common_index' do
-      expect { resource.to_common_index }.to raise_error(CommonIndexer::Error)
+      expect { index }.to raise_error(CommonIndexer::Error)
     end
   end
 
   context 'implemented' do
     before do
-      class TestModel < ActiveFedora::Base
-        include CommonIndexer::Base
-
-        property :title, predicate: ::RDF::URI('http://example.org/ns#title')
-        property :description, predicate: ::RDF::URI('http://example.org/ns#description')
-        property :collection, predicate: ::RDF::URI('http://example.org/ns#collection')
-        property :thumbnail, predicate: ::RDF::URI('http://example.org/ns#thumbnail')
-        property :poster, predicate: ::RDF::URI('http://example.org/ns#poster')
-        property :resource_type, predicate: ::RDF::URI('http://example.org/ns#resource_type')
-
+      class TestModel
         def to_common_index
-          {}
+          {
+            title: title
+          }
         end
       end
     end
 
-    after do
-      Object.send(:remove_const, :TestModel)
-    end
-
-    it 'includes CommonIndexer::Base' do
-      expect(TestModel.ancestors).to include(described_class)
-    end
-
-    context 'common index' do
-      it '#to_common_index' do
-        expect(resource.to_common_index).to be_a(Hash)
-      end
+    it '#to_common_index' do
+      expect(index).to eq(title: ['Main Title'])
     end
   end
 end
