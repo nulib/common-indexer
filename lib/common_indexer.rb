226d897bcc93d03920081bb8cda095befdf83709
Michael B. Klein <mbklein@gmail.com>
2018-08-23 16:08:58 -0500

    Use aliases and do on-the-fly reindexing

--- lib/common_indexer.rb
+++ lib/common_indexer.rb
@@ -26,13 +26,20 @@ module CommonIndexer # :nodoc:
     end
 
     def configure_index!
-      client.indices.create(index: index_name) unless client.indices.exists(index: index_name)
+      new_index = [index_name, Time.now.utc.strftime('%Y%m%d%H%M%S%3N')].join('_')
+      client.indices.create(index: new_index)
+      client.indices.put_mapping(index: new_index, type: '_doc', body: { _doc: config.schema })
+      reindex_into(new_index) if client.indices.exists(index: index_name)
+      client.indices.put_alias(index: new_index, name: index_name)
+    end
 
-      client.indices.put_mapping(
-        index: index_name,
-        type: '_doc',
-        body: { _doc: config.schema }
-      )
+    private
+
+    def reindex_into(new_index)
+      existing_index = client.indices.get(index: index_name).keys.first
+      raise ArgumentError, "Cannot reindex #{index_name} into itself" if existing_index == index_name
+      client.reindex(body: { source: { index: index_name }, dest: { index: new_index } })
+      client.indices.delete(index: existing_index)
     end
   end
 end
